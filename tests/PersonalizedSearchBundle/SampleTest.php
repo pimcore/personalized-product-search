f4b6919ca5e1fd75b92956418eb9d2e09f5678df
Dominik Blaha <dominik.blaha@outlook.at>
2020-04-29 13:06:30 +0200

    Fixes unittests

--- tests/PersonalizedSearchBundle/SampleTest.php
+++ tests/PersonalizedSearchBundle/SampleTest.php
@@ -8,16 +8,32 @@ use PHPUnit\Framework\TestCase;
 
 class SampleTest extends TestCase
 {
+
     protected function setUp() {
     }
 
-    public function testAdd()
-    {
-        $this->assertEquals(15, $this->calculator->add(10, 5));
+    public function testAdd() {
+        $this->assertEquals(15, 10 + 5);
     }
 
-    public function testAddWithProvider(int $a, int $b, int $expected) {
-        $result = $a + $b;
-        $this->assertEquals($expected, $result);
+    /**
+     * @dataProvider addDataProvider
+     */
+    public function testAddWithProvider(int $a, int $b, int $expected)
+    {
+        $this->assertEquals($expected, $a + $b);
+    }
+
+    public function addDataProvider(): array
+    {
+        return [
+            [1, 2, 3],
+            [10, 5, 15],
+            [-5, 5, 0],
+            [5, -5, 0],
+            [0, 10, 10],
+            [-50, -50, -100],
+            [-50, 10, -40]
+        ];
     }
 }
