68e41482f456e3688443334a40b26c4029cfeea1
EnNico <EnNico@users.noreply.github.com>
2020-05-06 11:42:39 +0200

    Modify commands and add entry in services.yaml

--- src/Command/ETLCommand.php
+++ src/Command/ETLCommand.php
@@ -3,8 +3,31 @@
 
 namespace PersonalizedSearchBundle\src\Command;
 
+use Pimcore\Bundle\PersonalizedSearchBundle\ExtractTransformLoad\PurchaseHistoryInterface;
+use Pimcore\Console\AbstractCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
-class ETLCommand
+class ETLCommand extends AbstractCommand
 {
+    private $purchaseHistory;
+    public function __construct(PurchaseHistoryInterface $purchaseHistory)
+    {
+        $this->purchaseHistory = $purchaseHistory;
+        parent::__construct();
+    }
 
+    public function configure()
+    {
+        $this
+            ->setName('personalizedsearch:start-etl')
+            ->setDescription('Manually triggers the ETL Mechanism');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->purchaseHistory->updateOrderIndexFromOrderDb();
+        $output->writeln('Manual invocation of ETL finished');
+    }
 }
--- src/Command/ETLCronCommand.php
+++ src/Command/ETLCronCommand.php
@@ -3,8 +3,37 @@
 
 namespace PersonalizedSearchBundle\src\Command;
 
+use Pimcore\Console\AbstractCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
-class ETLCronCommand
+
+class ETLCronCommand extends AbstractCommand
 {
+    public function configure()
+    {
+        $this
+            ->setName('personalizedsearch:schedule:etl')
+            ->setDescription('Registers the async invocation of the ETL')
+            ->addArgument('hours', InputArgument::REQUIRED, 'hour interval for the cron job')
+            ->addArgument('minutes', InputArgument::REQUIRED, 'minute interval for the cron job')
+            ->addArgument('startTime', InputArgument::OPTIONAL, 'the start time [hour]:[minute] when the cron job executes the first time, argument is optional and current time is taken as default');
+    }
 
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $hours = $input->getArgument('hours');
+        $minutes = $input->getArgument('minutes');
+        $startTime = $input->getArgument('startTime');
+
+        if($startTime) {
+            $output->writeln("custom startTime is set");
+        }
+
+        $commandResponse = shell_exec($minutes . " " . $hours . " * * * php bin/console personalizedsearch:start:etl &> /dev/null");
+
+        $output->writeln($commandResponse);
+        $output->writeln('Done with ETLCommand');
+    }
 }
