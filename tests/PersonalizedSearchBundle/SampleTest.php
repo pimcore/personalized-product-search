77dd435d8fa748eb6bdac82038e73e3ac594b6fe
Max <fatfrido@gmail.com>
2020-05-06 11:17:49 +0200

    Added an example for mocks in unit tests

--- tests/PersonalizedSearchBundle/SampleTest.php
+++ tests/PersonalizedSearchBundle/SampleTest.php
@@ -5,6 +5,8 @@
 namespace Pimcore\Bundle\PersonalizedSearchBundle\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Pimcore\Bundle\PersonalizedSearchBundle\IndexAccessProvider\IndexAccessProviderInterface;
+use Pimcore\Bundle\PersonalizedSearchBundle\IndexAccessProvider\OrderIndexAccessProvider;
 
 class SampleTest extends TestCase
 {
@@ -14,6 +16,29 @@ class SampleTest extends TestCase
 
     public function testAdd() {
         $this->assertEquals(15, 10 + 5);
+    }
+
+    public function testStub() {
+        // Create a stub for the OrderIndexAccessProvider class.
+        $stub = $this
+            -> getMockBuilder("OrderIndexAccessProvider")
+            -> setMethods(['fetchSegments'])
+            -> getMock();
+
+        // Configure the stub.
+        $stub->method('fetchSegments')
+            ->willReturn([
+                (object) [
+                    "segmentId" => 983,
+                    "segmentCount" => 1
+                ],
+                (object) [
+                    "segmentId" => 963,
+                    "segmentCount" => 2
+                ]
+            ]);
+
+        $this->assertSame(1, $stub->fetchSegments(1021)[0]->segmentCount);
     }
 
     /**
