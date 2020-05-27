4276eec52d84543b1eda3ca13d37996bda576a33
Lukas Schoerghuber <lukas.schoerghuber@posteo.at>
2020-05-27 12:34:22 +0200

    Remove dependency to e-commerce Factory
    This commit moves the ETL mechanism's dependency from the instance of
    the class `OrderManager` provided via the class `Factory` of the
    Pimcore ecommerce bundle to a provider for `OrderManager` which can be
    supplied via DI.

---
+++ src/ExtractTransformLoad/PersonalizationOrderManagerProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Pimcore\Bundle\PersonalizedSearchBundle\ExtractTransformLoad;
+
+
+use Pimcore\Bundle\EcommerceFrameworkBundle\OrderManager\OrderManagerInterface;
+
+interface PersonalizationOrderManagerProvider
+{
+    function getOrderManager(): OrderManagerInterface;
+}
