88fc3302d4143c116bb5569881e529380bce3e4f
Max <fatfrido@gmail.com>
2020-05-27 12:16:22 +0200

    Added date and time to measurements

--- src/Decorator/PerformanceMeasurementDecorator.php
+++ src/Decorator/PerformanceMeasurementDecorator.php
@@ -11,10 +11,10 @@ class PerformanceMeasurementDecorator extends AbstractDecorator
 {
     const FILE_NAME = "/home/pimcoredemo/AdapterPerformance.txt";
 
-    /*function __construct() {
+    function __construct() {
         parent::__construct();
-        file_put_contents(self::FILE_NAME, "");
-    }*/
+        file_put_contents(self::FILE_NAME, "-----------------------------------------\r\n", FILE_APPEND);
+    }
 
     protected function invokeAdapter(AdapterInterface $adapter, array $query): array
     {
@@ -22,7 +22,9 @@ class PerformanceMeasurementDecorator extends AbstractDecorator
         $res = $adapter->addPersonalization($query);
         $time_elapsed_secs = microtime(true) - $start;
 
-        file_put_contents(self::FILE_NAME,  get_class($adapter) . ": " . $time_elapsed_secs * 1000 . "ms" . "\r\n", FILE_APPEND);
+        file_put_contents(self::FILE_NAME,
+            date('Y-m-d H:i:s') . ": " . get_class($adapter) . ": " . $time_elapsed_secs * 1000 . "ms" . "\r\n",
+            FILE_APPEND);
 
         return $res;
     }
