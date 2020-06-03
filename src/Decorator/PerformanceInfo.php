5b02de5d53af8cf60be1867c7b156cbb83a86a3a
Dominik Blaha <dominik.blaha@outlook.at>
2020-06-03 12:17:46 +0200

    Adds timestamp to PerformanceInfo

--- src/Decorator/PerformanceInfo.php
+++ src/Decorator/PerformanceInfo.php
@@ -6,10 +6,12 @@ class PerformanceInfo
 {
     public $adapterName;
     public $elapsedTime;
+    public $timestamp;
 
     public function __construct($adapterName, $elapsedTime)
     {
         $this->adapterName = $adapterName;
         $this->elapsedTime = $elapsedTime;
+        $this->timestamp = date("Y-m-d H:i:s");
     }
 }
