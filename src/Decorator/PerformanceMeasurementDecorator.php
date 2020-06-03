1858585e64a2f2c10354d0ccd60c4214ca6aa673
Dominik Blaha <dominik.blaha@outlook.at>
2020-06-03 13:21:09 +0200

    Finalize performance measuring

--- src/Decorator/PerformanceMeasurementDecorator.php
+++ src/Decorator/PerformanceMeasurementDecorator.php
@@ -8,7 +8,6 @@ use Pimcore\Bundle\PersonalizedSearchBundle\IndexAccessProvider\AdapterPerforman
 
 class PerformanceMeasurementDecorator extends AbstractDecorator
 {
-    const FILE_NAME = "/home/pimcoredemo/AdapterPerformance.txt";
 
     /**
      * @var AdapterPerformanceIndexAccessProvider
@@ -18,7 +17,6 @@ class PerformanceMeasurementDecorator extends AbstractDecorator
     function __construct(AdapterPerformanceIndexAccessProvider $adapterPerformanceIndex) {
         parent::__construct();
         $this->adapterPerformanceIndex = $adapterPerformanceIndex;
-//        file_put_contents(self::FILE_NAME, "-----------------------------------------\r\n", FILE_APPEND);
     }
 
     protected function invokeAdapter(AdapterInterface $adapter, array $query): array
@@ -29,14 +27,6 @@ class PerformanceMeasurementDecorator extends AbstractDecorator
 
         $performanceInfo = new PerformanceInfo(get_class($adapter), $elapsedTimeInSeconds * 1000);
         $this->adapterPerformanceIndex->index(0, $performanceInfo);
-
-//        print_r("--- before print\n");
-//        $hugo = date('Y-m-d H:i:s') . ": " . get_class($adapter) . ": " . $elapsedTimeInSeconds * 1000 . "ms" . "\n";
-//        print_r("text: " . $hugo . "\n");
-//        file_put_contents(self::FILE_NAME,
-//            $hugo,
-//            FILE_APPEND);
-//        print_r("after print --- \n");
 
         return $res;
     }
