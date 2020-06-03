9586b935797b01c5471cfb8293f16acf318418cb
Dominik Blaha <dominik.blaha@outlook.at>
2020-06-03 10:21:02 +0200

    Adds index for performance measuring

--- src/Decorator/PerformanceMeasurementDecorator.php
+++ src/Decorator/PerformanceMeasurementDecorator.php
@@ -4,25 +4,51 @@
 namespace Pimcore\Bundle\PersonalizedSearchBundle\Decorator;
 
 use Pimcore\Bundle\PersonalizedSearchBundle\Adapter\AdapterInterface;
+use Pimcore\Bundle\PersonalizedSearchBundle\IndexAccessProvider\AdapterPerformanceIndexAccessProvider;
+
+class PerformanceInfo
+{
+    public $adapterName;
+    public $elapsedTime;
+
+    public function __construct($adapterName, $elapsedTime)
+    {
+        $this->adapterName = $adapterName;
+        $this->elapsedTime = $elapsedTime;
+    }
+}
 
 class PerformanceMeasurementDecorator extends AbstractDecorator
 {
     const FILE_NAME = "/home/pimcoredemo/AdapterPerformance.txt";
 
-    function __construct() {
+    /**
+     * @var AdapterPerformanceIndexAccessProvider
+     */
+    private $adapterPerformanceIndex;
+
+    function __construct(AdapterPerformanceIndexAccessProvider $adapterPerformanceIndex) {
+        $this->adapterPerformanceIndex = $adapterPerformanceIndex;
         parent::__construct();
-        file_put_contents(self::FILE_NAME, "-----------------------------------------\r\n", FILE_APPEND);
+//        file_put_contents(self::FILE_NAME, "-----------------------------------------\r\n", FILE_APPEND);
     }
 
     protected function invokeAdapter(AdapterInterface $adapter, array $query): array
     {
         $start = microtime(true);
         $res = $adapter->addPersonalization($query);
-        $time_elapsed_secs = microtime(true) - $start;
+        $elapsedTimeInSeconds = microtime(true) - $start;
 
-        file_put_contents(self::FILE_NAME,
-            date('Y-m-d H:i:s') . ": " . get_class($adapter) . ": " . $time_elapsed_secs * 1000 . "ms" . "\r\n",
-            FILE_APPEND);
+        $performanceInfo = new PerformanceInfo(get_class($adapter), $elapsedTimeInSeconds * 1000);
+        $this->adapterPerformanceIndex->index(0, $performanceInfo);
+
+//        print_r("--- before print\n");
+//        $hugo = date('Y-m-d H:i:s') . ": " . get_class($adapter) . ": " . $elapsedTimeInSeconds * 1000 . "ms" . "\n";
+//        print_r("text: " . $hugo . "\n");
+//        file_put_contents(self::FILE_NAME,
+//            $hugo,
+//            FILE_APPEND);
+//        print_r("after print --- \n");
 
         return $res;
     }
