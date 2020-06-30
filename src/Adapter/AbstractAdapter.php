7ec2c4eeec164f33fa390902a288b7f0f7cc1476
Christian Fasching <christian.fasching@pimcore.com>
2020-06-30 13:28:28 +0200

    improve debug info

--- src/Adapter/AbstractAdapter.php
+++ src/Adapter/AbstractAdapter.php
@@ -3,6 +3,29 @@
 
 namespace Pimcore\Bundle\PersonalizedSearchBundle\Adapter;
 
+use CustomerManagementFrameworkBundle\SegmentManager\SegmentManagerInterface;
+
 abstract class AbstractAdapter implements AdapterInterface
 {
+
+    /**
+     * @var SegmentManagerInterface
+     */
+    protected $segmentManager;
+
+    public function __construct(SegmentManagerInterface $segmentManager) {
+        $this->segmentManager = $segmentManager;
+    }
+
+    protected function getSegmentName(int $segmentId): string {
+
+        $segment = $this->segmentManager->getSegmentById($segmentId);
+        if($segment) {
+            return $segment->getName();
+        }
+
+        return '';
+    }
+
+
 }
